8b0a0ebd1ac40fb774046532aa4ac23c52246bd9
leesb <echo3042@gmail.com>
2024-04-09 16:36:55 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+function handleClickSubmit() {
+    window.alert("Your requeest has been submitted. We will get back to you soon. You will be redirected to the home page.");
+    window.location.href = "/";
+}
